8d3211492fd3b65034db9bf8129bbeb12321d54c
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2020-10-04 00:05:06 -0300

    Add files via upload

---
+++ Lista3/Lista3_Q2_Comparar strings.c
@@ -0,0 +1,37 @@
+//Implemente a função strcmp de string.h utilizando qualquer estrutura de repetição.
+//A função strcmp compara os conteúdos de duas strings e verifica se são iguais retornando 0.
+
+#include <stdio.h>
+
+int main () {
+
+	char s1[20];
+	char s2[20];
+	int i;
+	int cont = 0;
+
+    printf("Digite duas palavras (Max 20 letras):\n");
+	scanf("%s", s1);
+	scanf("%s", s2);
+
+	for(i = 0; s1[i] != '\0' && i <= sizeof(s1) ; i++) {
+		if(s1[i] != s2[i]){
+		cont++;
+		break;
+		}
+	}
+	for(i = 0; s2[i] != '\0' && i <= sizeof(s2) ; i++) {
+		if(s2[i] != s1[i]){
+		cont++;
+		break;
+		}
+	}
+
+	if (cont == 0) {
+		puts("As palavras sao iguais. Retorno: 0");
+	} else {
+		puts("As palavras sao diferentes.");
+	}
+
+	return 0;
+}
---
+++ Lista3/Lista3_Q3_Concatenar strings.c
@@ -0,0 +1,27 @@
+//Implemente a função strcat de string.h utilizando qualquer estrutura de repetição.
+//A função strcat concatena o conteúdo de uma string em outra.
+//DICA: Cuidado com o espaço em memória.
+
+#include <stdio.h>
+
+int main () {
+
+	char s1[40], s2[20];
+	int i , j = 0;
+
+	printf("Digite duas palavras (Max 20 letras cada):\n");
+	scanf("%s", s1);
+	scanf("%s", s2);
+
+	//descobrir posição do ultimo caractere da primeira string
+	for(i = 0; s1[i] != '\0' && i <= (sizeof(s1)); i++) {}
+
+    //passar valores da segunda string para a primeira a partir do ultimo caractere da primeira string
+    for(j = 0; s2[j] != '\0' && j <= (sizeof(s2)); j++, i++) {
+        s1[i] = s2[j];
+    }
+
+	printf("A palavra concatenada eh: %s", s1);
+
+	return 0;
+}
