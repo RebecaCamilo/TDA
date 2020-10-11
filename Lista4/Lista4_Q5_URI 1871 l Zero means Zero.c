09e5141e93c4c803b3455b07101f2f7fd46dd741
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2020-10-11 20:00:25 -0300

    Add files via upload

---
+++ Lista4/Lista4_Q5_URI 1871 l Zero means Zero.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+
+    int n1, n2, soma, i;
+    int s[15];
+
+    scanf("%d %d", &n1, &n2);
+    soma = n1 + n2;
+
+
+//Separa cada dígito do número e coloca em posições do vetor;
+
+//verificar cada numero da soma e usar if para printar apenas (s[i] != 0)
+
+	return 0;
+}
