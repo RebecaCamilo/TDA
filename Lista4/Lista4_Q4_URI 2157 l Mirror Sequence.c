6e9faaa570b9915b54ce4085045f33a91afd436d
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2020-10-12 15:14:48 -0300

    Update Lista4_Q4_URI 2157 l Mirror Sequence.c

--- Lista4/Lista4_Q4_URI 2157 l Mirror Sequence.c
+++ Lista4/Lista4_Q4_URI 2157 l Mirror Sequence.c
@@ -1,5 +1,5 @@
 /*
-Sequência Espelho
+URI 2157 | Sequência Espelho
 Imprimir números em sequência é uma tarefa relativamente simples. Mas, e quando se trata de uma sequência espelho?
 Trata-se de uma sequência que possui um número de início e um número de fim, e todos os números entre estes, inclusive estes,
 são dispostos em uma sequência crescente, sem espaços e, em seguida, esta sequência é projetada de forma invertida,
