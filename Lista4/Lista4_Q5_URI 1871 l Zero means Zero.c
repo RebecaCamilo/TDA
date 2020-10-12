64d6e4feb4b4a6ba9f9c90b9f26e4576d37bc346
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2020-10-12 19:27:20 -0300

    Update Lista4_Q5_URI 1871 l Zero means Zero.c

--- Lista4/Lista4_Q5_URI 1871 l Zero means Zero.c
+++ Lista4/Lista4_Q5_URI 1871 l Zero means Zero.c
@@ -11,16 +11,20 @@ Escreva um programa que, dado dois números inteiros, sem o algarismo zero, some
 
 int main () {
 
-    int n1, n2, soma, i;
-    int s[15];
+    int n1, n2, soma, i = 0, array[30];
 
-    scanf("%d %d", &n1, &n2);
-    soma = n1 + n2;
-
-
-//Separa cada dígito do número e coloca em posições do vetor;
-
-//verificar cada numero da soma e usar if para printar apenas (s[i] != 0)
-
-	return 0;
+    while(scanf("%d %d", &n1, &n2) && n1 != 0 || n2 != 0) {
+        soma = n1 + n2;
+        while(soma) {
+            array[i] = soma % 10;
+            soma /= 10;
+            i++;
+        }
+        for(i -= 1; i >= 0; i--) {
+            if(array[i])
+                printf("%d", array[i]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
