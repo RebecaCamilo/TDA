3197b3ed730fb49556850ee5f9689d3479f6ae54
Rebeca Maria Fialho Camilo <71824475+RebecaCamilo@users.noreply.github.com>
2020-10-12 15:13:18 -0300

    Update Lista4_Q1_URI 1000 l Hello World.c

--- Lista4/Lista4_Q1_URI 1000 l Hello World.c
+++ Lista4/Lista4_Q1_URI 1000 l Hello World.c
@@ -1,4 +1,8 @@
-//URI 1000 | Hello World!
+/*
+URI 1000 | Hello World!
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+*/
 
 #include <stdio.h>
 
